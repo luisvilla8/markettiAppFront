a8c293505ef9cf513784009ae712568c7fe885dd
Luis Villacorta <lvillacorta@itereo.travel>
2023-06-05 00:06:07 -0500

    chore: adds font size prop for tittle component

--- src/components/base/Title/Title.tsx
+++ src/components/base/Title/Title.tsx
@@ -2,11 +2,14 @@ import { ReactNode } from "react"
 import styles from './Title.module.css'
 
 type Props = {
+  fontSize?: string
   children: ReactNode
 }
 
-export const Title = ({ children }: Props) => {
+export const Title = ({ fontSize = "2rem", children }: Props) => {
   return (
-    <h1 className={styles.title}>{ children }</h1>
+    <h1 className={styles.title} style={{ fontSize }}>
+      { children }
+    </h1>
   )
 }
