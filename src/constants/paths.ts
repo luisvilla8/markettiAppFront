ef56af7ee3e72385d56eed8bd4068b1938b59264
Luis Villacorta <lvillacorta@itereo.travel>
2023-06-29 00:48:54 -0500

    chore: adds constants files

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,5 +1,58 @@
 export const PATHS = {
-  HOME: '/',
-  SIGNIN: '/sign-in',
-  SIGNUP: '/sign-up',
+  HOME: {
+    path: '/',
+    name: 'Home',
+    label: 'Home',
+  },
+  SIGNIN: {
+    path: '/sign-in',
+    name: 'Sign In',
+    label: 'Sign In',
+  },
+  SIGNUP: {
+    path: '/sign-up',
+    name: 'Sign Up',
+    label: 'Sign Up',
+  },
+  CREATE_SURVEY: {
+    path: '/create-survey',
+    name: 'Create Survey',
+    label: 'Create Survey',
+  },
+  SURVEYS: {
+    path: '/surveys',
+    name: 'Surveys',
+    label: 'Surveys',
+  },
+  TEAM_USERS: {
+    path: '/team-users',
+    name: 'Team Users',
+    label: 'Team Users',
+  },
+  PROFILE: {
+    path: '/profile',
+    name: 'Profile',
+    label: 'Profile',
+  },
+  SETTINGS: {
+    path: '/settings',
+    name: 'Settings',
+    label: 'Settings',
+  },
+  LOGOUT: {
+    path: '/logout',
+    name: 'Logout',
+    label: 'Logout',
+  },
 }
+
+export const NAVBAR_PATHS_BASE = {
+  HOME: PATHS.HOME,
+  SURVEYS: PATHS.SURVEYS,
+  TEAM_USERS: PATHS.TEAM_USERS,
+  PROFILE: PATHS.PROFILE,
+  SETTINGS: PATHS.SETTINGS,
+  LOGOUT: PATHS.LOGOUT,
+}
+
+export const NAVBAR_PATHS = Object.values(NAVBAR_PATHS_BASE)
