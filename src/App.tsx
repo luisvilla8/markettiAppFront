886dd9741dbf921e23d375ff24038ca76a4cf822
Luis Villacorta <lvillacorta@itereo.travel>
2023-06-29 00:58:31 -0500

    chore: addjusts app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Home, SignIn, SignUp } from "@/views"
+import { CreateSurvey, Home, Logout, SignIn, SignUp } from "@/views"
 import { PATHS, THEMES } from "@/constants"
 import { MustAuth, NoMustAuth } from "@/components"
 import './styles/variables.css'
@@ -15,11 +15,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<MustAuth />}>
-            <Route path={PATHS.HOME} element={<Home/>} />
+            <Route path={PATHS.HOME.path} element={<Home/>} />
+            <Route path={PATHS.CREATE_SURVEY.path} element={<CreateSurvey/>} />
+            <Route path={PATHS.LOGOUT.path} element={<Logout/>} />
           </Route>
           <Route element={<NoMustAuth />}>
-            <Route path={PATHS.SIGNIN} element={<SignIn />} />
-            <Route path={PATHS.SIGNUP} element={<SignUp />} />
+            <Route path={PATHS.SIGNIN.path} element={<SignIn />} />
+            <Route path={PATHS.SIGNUP.path} element={<SignUp />} />
           </Route>
         </Routes>
       </BrowserRouter>
