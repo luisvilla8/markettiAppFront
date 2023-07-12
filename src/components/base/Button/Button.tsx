9b50484d4f3e54ad6d5c0ff5155cc41f8552fa48
Luis Villacorta <lvillacorta@itereo.travel>
2023-07-12 00:20:18 -0500

    fix(#9): adjusts props and styles of button

--- src/components/base/Button/Button.tsx
+++ src/components/base/Button/Button.tsx
@@ -6,15 +6,23 @@ type Props = {
   children: ReactNode
   disabled?: boolean
   type?: "submit" | "button"
-  style?: object
+  style?: {
+    role?: "cancel" | "confirm" | "highlight"
+    styles?: React.CSSProperties
+  }
   onClick?: () => void
 }
 
-export const Button = ({ children, onClick, style = {}, disabled = false, type = "submit" }: Props) => {
+export const Button = ({ children, onClick, style = {}, disabled = false, type = "button" }: Props) => {
+
+  const role = style.role ?? ""
+
+  const className = `${styles.button__container} ${styles[role]}`
+
   return (
     <motion.button
-      className={styles.button__container}
-      style={style}
+      className={className}
+      style={style.styles}
       type={type}
       disabled={disabled}
       whileTap={{ scale: 0.98 }}
